8316b04bf75ac32a957ac5e134bc3684c0086333
Krish Kapoor <krishk122703@gmail.com>
2025-12-17 22:16:16 +0000

    some major changes underway this is just a precautionary commit

--- src/dataloader/transformer_dataloader.py
+++ src/dataloader/transformer_dataloader.py
@@ -4,12 +4,7 @@ from torch.utils.data import DataLoader
 from src.dataset.transformer_dataset import TransformerDataset
 
 
-def TransformerDataLoader(data_path, config):
-    #create datasets:
-    train_dataset = TransformerDataset(data_path, split='train')
-    eval_dataset = TransformerDataset(data_path, split='eval')
-    test_dataset = TransformerDataset(data_path, split='test')
-
+def TransformerDataLoader(data_path, config, split='train'):
     #pin memory for faster gpu transfer if cuda available:
     pin_memory = torch.cuda.is_available()
 
@@ -18,27 +13,35 @@ def TransformerDataLoader(data_path, config):
     workers = int(config['training']['num_workers'])
 
     #loaders:
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size = batch_size,
-        shuffle=True,
-        num_workers = workers,
-        pin_memory = pin_memory
-    )
+    if split == 'train':
+        train_dataset = TransformerDataset(data_path, split='train') # train dataset instance
+        train_loader = DataLoader(
+            train_dataset,
+            batch_size = batch_size,
+            shuffle=True,
+            num_workers = workers,
+            pin_memory = pin_memory
+        )
+        return train_loader #returns only train loader
 
-    eval_loader = DataLoader(
-        eval_dataset,
-        batch_size = batch_size,
-        shuffle=False,
-        num_workers = workers,
-        pin_memory = pin_memory
-    )
+    if split == 'eval':
+        eval_dataset = TransformerDataset(data_path, split='eval') # eval dataset instance
+        eval_loader = DataLoader(
+            eval_dataset,
+            batch_size = batch_size,
+            shuffle=False,
+            num_workers = workers,
+            pin_memory = pin_memory
+        )
+        return eval_loader #returns only eval loader
 
-    test_loader = DataLoader(
-        test_dataset,
-        batch_size = batch_size,
-        shuffle = False,
-        num_workers = workers,
-        pin_memory = pin_memory
-    )
-    return train_loader, eval_loader, test_loader
+    if split == 'test':
+        test_dataset = TransformerDataset(data_path, split='test') # test dataset instance
+        test_loader = DataLoader(
+            test_dataset,
+            batch_size = batch_size,
+            shuffle = False,
+            num_workers = workers,
+            pin_memory = pin_memory
+        )
+        return test_loader #returns only test loader
